f1a357404d10859e26b31e61665f9455e3582300
Marlon van der Linde <marlon250f@gmail.com>
2016-09-12 00:31:47 +0200

    Config needs a rethink...

--- src/tuning.js
+++ src/tuning.js
@@ -1,5 +1,10 @@
 "use strict";
 
+
+/**
+ * Tuning is (supposed to be) a remote config retriever.
+ * Configuration is stored remotely, and downloaded into a local JSON config, which local code can read from.
+ */
 class Tuning {
 
     constructor() {

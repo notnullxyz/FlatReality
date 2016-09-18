176f802831b25a2951ef15e1b6f007ca40d67a1f
Marlon van der Linde <marlon250f@gmail.com>
2016-09-18 17:43:15 +0200

    Loader tested with textures loading for both the ground plane and the crates. Promises are just great!

--- src/main.js
+++ src/main.js
@@ -3,6 +3,23 @@
 let Setup = require("./setup");
 let GameLoop = require("./gameloop");
 
+function stat() {
+    var script=document.createElement('script');
+    script.onload=function() {
+        var stats=new Stats();
+        document.body.appendChild(stats.dom);
+        requestAnimationFrame( function loop() {
+            stats.update();
+            requestAnimationFrame(loop)
+        });
+    };
+    script.src='//rawgit.com/mrdoob/stats.js/master/build/stats.min.js';
+    document.head.appendChild(script);
+}
+
+stat();
+
+
 // Create EMPTY WORLD
 let s = new Setup();
 s.init().then((vitals) => {
